5bd6e065cb869557392405124ed18586a0636376
MacBookPro <kayro-11@outlook.com>
2024-01-22 18:47:14 +0800

    modified:   window/draw.py

--- window/draw.py
+++ window/draw.py
@@ -11,7 +11,6 @@ window.geometry('500x300')
 #创建标签
 MainLabel=tk.Label(window,text=MainLabel,bg='grey',font='Arial,12',width=100,height=1)
 MainLabel.pack()
-
 #创建输入窗口
 entry1=tk.Entry(window,show=None,font=('Arial',14),width=20)
 entry1.place(x=20,y=50)
